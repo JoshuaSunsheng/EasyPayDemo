980bcc7b314c167a0050d4e658e9a2d5b7e18a53
su371128017 <su371128017@163.com>
2019-05-13 11:29:21 +0800

    nie 方法提交

---
+++ src/com/easypay/NopagesMain.java
@@ -0,0 +1,131 @@
+package com.easypay;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import net.sf.json.JSONObject;
+
+/**
+ * 无跳转快捷测试
+ * @author njp
+ *
+ */
+public class NopagesMain {
+
+	//标记生产还是测试环境
+    public static boolean isTest = true;
+
+    //根据接口文档生成对应的json请求字符串
+    private static String biz_content = "";
+
+    //接口文档中的方法名
+    private static String service = "easypay.pay.nopages.sendSMS";
+
+    //商户号
+    private static String merchant_id = KeyUtils.TEST_DEFAULT_MERCHANT_ID;
+
+    //接入机构号
+    private static String partner = KeyUtils.TEST_DEFAULT_PARTNER;
+
+    //请求地址
+    private static String url = KeyUtils.DEFAULT_URL;
+
+    //key密钥
+    private static String key = KeyUtils.TEST_MERCHANT_PRIVATE_KEY;
+
+    //加密密钥
+    private static String DES_ENCODE_KEY = KeyUtils.TEST_DES_ENCODE_KEY;
+
+    //无跳转快捷获取验证码
+    public static void sendSMS(){
+        JSONObject sParaTemp = new JSONObject();
+        sParaTemp.put("merchant_id", merchant_id);
+        sParaTemp.put("out_trade_no", KeyUtils.getOutTradeNo());
+        sParaTemp.put("amount", "1");
+        sParaTemp.put("acc", getEncode("6225768759941717"));   //银行卡号
+        sParaTemp.put("mobile", getEncode("18010461286")); //手机号
+        sParaTemp.put("cvv", getEncode("427"));
+        sParaTemp.put("validity_year", getEncode("20"));
+        sParaTemp.put("validity_month", getEncode("12"));
+        sParaTemp.put("subject", "subject");
+        sParaTemp.put("body", "body");
+        sParaTemp.put("seller_email", "18679106330@gmail.com");
+        sParaTemp.put("notify_url", "https://www.baidu.com");
+
+        biz_content = sParaTemp.toString();
+
+        service  = "easypay.pay.nopages.sendSMS";
+    }
+
+
+    private static String getEncode(String data){
+        return StringUtils.bytesToHexStr(DesUtil.desEncode(data, DES_ENCODE_KEY));
+    }
+
+    //银联无跳转支付
+    public static void pay(String orderId,String vcode){
+        JSONObject sParaTemp = new JSONObject();
+        sParaTemp.put("merchant_id", merchant_id);
+        sParaTemp.put("out_trade_no", orderId);
+        sParaTemp.put("amount", 1);
+        sParaTemp.put("acc", getEncode("6225768759941717"));   //银行卡号
+        sParaTemp.put("mobile", getEncode("18010461286")); //手机号
+        sParaTemp.put("vcode", vcode);
+
+        biz_content = sParaTemp.toString();
+        service  = "easypay.pay.nopages.pay";
+    }
+
+    public static void main(String[] args) {
+        //易生请求示例子
+        try {
+
+            //系统入件之后生成的合作伙伴ID（一般会通过邮件发送）
+            if (!isTest) {
+                //商户号
+                merchant_id = KeyUtils.SC_DEFAULT_MERCHANT_ID;
+                //接入机构号
+                partner = KeyUtils.SC_DEFAULT_PARTNER;
+                //请求地址
+                url = KeyUtils.SC_URL;
+                //key密钥
+                key = KeyUtils.SC_MERCHANT_PRIVATE_KEY;
+                //加密密钥
+                DES_ENCODE_KEY = KeyUtils.SC_DES_ENCODE_KEY;
+            }
+
+            //无跳转快捷获取验证码
+//            sendSMS();
+
+
+            //银联无跳转支付
+            pay("201904241556100526887","276366");
+
+            //加密类型，默认RSA
+            String sign_type = KeyUtils.TEST_DEFAULT_ENCODE_TYPE;
+            //编码类型
+            String charset = KeyUtils.TEST_DEFAULT_CHARSET;
+
+            //根据请求参数生成的机密串
+            String sign = KeyUtils.getSign(KeyUtils.TEST_MERCHANT_PRIVATE_KEY, charset, biz_content);
+            System.out.print("计算签名数据为：" + sign + "\n");
+            Map<String, String> reqMap = new HashMap<String, String>(6);
+            reqMap.put("biz_content", biz_content);
+            reqMap.put("service", service);
+            reqMap.put("partner", partner);
+            reqMap.put("sign_type", sign_type);
+            reqMap.put("charset", charset);
+            reqMap.put("sign", sign);
+
+            StringBuilder resultStrBuilder = new StringBuilder();
+            int ret = HttpConnectUtils.sendRequest(url, KeyUtils.TEST_DEFAULT_CHARSET, reqMap, 30000, 60000, "POST", resultStrBuilder, null);
+            System.out.print(" \n请求地址为：" + url +
+                    "\n 请求结果为：" + ret +
+                    "\n 请求参数为：" + reqMap.toString() +
+                    "\n 返回内容为：" + resultStrBuilder.toString() + "\n");
+        }catch (Exception e){
+            System.out.print(e.getMessage()+ "\n");
+        }
+    }
+
+}
