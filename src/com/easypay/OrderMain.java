31ea89f90985bbbad84c88a04cf0d9ef8dc71df1
su371128017 <su371128017@163.com>
2019-06-17 14:47:46 +0800

    公众号支付 demo 更新

--- src/com/easypay/OrderMain.java
+++ src/com/easypay/OrderMain.java
@@ -5,7 +5,8 @@ import net.sf.json.JSONObject;
 import java.util.HashMap;
 import java.util.Map;
 
-//新无卡测试demo
+//二维码，公众号测试demo
+//包含退款和订单查询
 public class OrderMain {
 
     //标记生产还是测试环境
@@ -26,17 +27,23 @@ public class OrderMain {
     //请求地址
     private static String url = KeyUtils.DEFAULT_URL;
 
-    //商户私钥
+    //key密钥
     private static String key = KeyUtils.TEST_MERCHANT_PRIVATE_KEY;
-
-    //易生公钥
-    private static String easypay_pub_key = KeyUtils.TEST_EASYPAY_PUBLIC_KEY;
 
     //加密密钥
     private static String DES_ENCODE_KEY = KeyUtils.TEST_DES_ENCODE_KEY;
 
     //二维码下单
     public static void qrcodePayPush(String payType){
+        JSONObject sParaTemp = qrcodeAndJsPayPush(payType);
+        sParaTemp.put("pay_acc_type", "00");
+
+        biz_content = sParaTemp.toString();
+
+        service  = "easypay.qrcode.pay.push";
+    }
+
+    public static JSONObject qrcodeAndJsPayPush(String payType) {
         JSONObject sParaTemp = new JSONObject();
         sParaTemp.put("merchant_id", merchant_id);
         sParaTemp.put("seller_email", "18679106330@gmail.com");
@@ -44,53 +51,26 @@ public class OrderMain {
         sParaTemp.put("business_time", "2017-12-07 15:35:00");
         sParaTemp.put("notify_url", "https://www.baidu.com");
         sParaTemp.put("order_desc", "Echannell");
+        sParaTemp.put("subject", "Echannell");
         sParaTemp.put("pay_type", payType);
 
-        sParaTemp.put("out_trade_no", KeyUtils.getOutTradeNo());
+        sParaTemp.put("out_trade_no", "demo" + KeyUtils.getOutTradeNo() + "_");
+        return sParaTemp;
+    }
+
+    public static void jsPayPush(String payType, String open_id){
+        JSONObject sParaTemp = qrcodeAndJsPayPush(payType);
+        sParaTemp.put("open_id", open_id );
         biz_content = sParaTemp.toString();
 
-        service  = "easypay.qrcode.pay.push";
-    }
-    //二维码下单
-    public static void netBankPay(){
-        JSONObject reqMap = new JSONObject();
-        reqMap.put("subject", "测试订单");
-        reqMap.put("body", "测试订单");
-        reqMap.put("merchant_id", merchant_id);
-        reqMap.put("out_trade_no",  KeyUtils.getOutTradeNo());
-        reqMap.put("bank_code", "ICBC");
-        reqMap.put("account_type", "1");
-        reqMap.put("amount", "1");
-        reqMap.put("front_url", "https://www.baidu.com");
-        reqMap.put("notify_url", "https://www.baidu.com");
-
-        biz_content = reqMap.toString();
-
-        service  = "easypay.merchant.netBankPay";
+        service  = "easypay.js.pay.push";
     }
 
-    public static void easyPay(){
-        JSONObject reqMap = new JSONObject();
-        reqMap.put("subject", "测试订单");
-        reqMap.put("body", "测试订单");
-        reqMap.put("merchant_id", merchant_id);
-        reqMap.put("out_trade_no",  KeyUtils.getOutTradeNo());
-        reqMap.put("bank_code", "ICBC");
-        reqMap.put("account_type", "1000");
-        reqMap.put("amount", "1");
-        reqMap.put("order_type", "100");
-        reqMap.put("front_url", "https://www.baidu.com");
-        reqMap.put("notify_url", "https://www.baidu.com");
 
-        biz_content = reqMap.toString();
-
-        service  = "easypay.merchant.easyPay";
-    }
-
-    //订单查询
+    //新无卡-协议支付-账户签约
     public static void orderQuery(String out_trade_no){
         JSONObject sParaTemp = new JSONObject();
-        sParaTemp.put("merchant_id", "900029000002554");
+        sParaTemp.put("merchant_id", merchant_id);
 
         sParaTemp.put("out_trade_no", out_trade_no);
 
@@ -103,7 +83,7 @@ public class OrderMain {
         JSONObject sParaTemp = new JSONObject();
         sParaTemp.put("merchant_id", merchant_id);
         sParaTemp.put("refund_amount", "1");
-        sParaTemp.put("out_trade_no", KeyUtils.getOutTradeNo());
+        sParaTemp.put("out_trade_no", "demo" + KeyUtils.getOutTradeNo());
         sParaTemp.put("origin_trade_no", origin_trade_no);
         sParaTemp.put("subject", "testRefund");
 
@@ -123,29 +103,24 @@ public class OrderMain {
                 partner = KeyUtils.SC_DEFAULT_PARTNER;
                 //请求地址
                 url = KeyUtils.SC_URL;
-                //商户私钥
+                //key密钥
                 key = KeyUtils.SC_MERCHANT_PRIVATE_KEY;
-                //易生公钥
-                easypay_pub_key = KeyUtils.SC_EASYPAY_PUBLIC_KEY;
                 //加密密钥
                 DES_ENCODE_KEY = KeyUtils.SC_DES_ENCODE_KEY;
             }
 
             //二维码订单推送
-//            OrderMain.qrcodePayPush("wxNative");//unionNative
+//            OrderMain.qrcodePayPush("aliPay");//银联：unionNative, 微信：wxNative, 支付宝：aliPay
 
-            //直连网银推单
-            OrderMain.netBankPay();
-
-            //标准收银台推单
-//            OrderMain.easyPay();
+            //公众号订单推送
+            OrderMain.jsPayPush("wxJsPay","oVRQJ05dzTQ7PO6qlST36ibnw8X8");//wxJsPay
+//            OrderMain.jsPayPush("aliJsPay","20881007434917916336963360919773");// aliJsPay
 
             //订单查询
-//            OrderMain.orderQuery("20190417c2bPay1555489983807");
+//            OrderMain.orderQuery("demo1553480416547");
 
             //订单退款
 //            OrderMain.refund("2018060114615570");
-
 
             //加密类型，默认RSA
             String sign_type = KeyUtils.TEST_DEFAULT_ENCODE_TYPE;
@@ -163,29 +138,18 @@ public class OrderMain {
             reqMap.put("charset", charset);
             reqMap.put("sign", sign);
 
-            if (service == "easypay.merchant.netBankPay" || service == "easypay.merchant.easyPay") {
-                //建议使用form表单post提交推单请求
-                System.out.println("Form请求html: \n");
-                StringUtils.createAutoFormHtml(url, reqMap, "UTF-8");
-            } else {
-                StringBuilder resultStrBuilder = new StringBuilder();
-                int ret = HttpConnectUtils.sendRequest(url, KeyUtils.TEST_DEFAULT_CHARSET, reqMap, 30000, 60000, "POST", resultStrBuilder, null);
-                System.out.print(" \n请求地址为：" + url +
-                        "\n 请求结果为：" + ret +
-                        "\n 请求参数为：" + reqMap.toString() +
-                        "\n 返回内容为：" + resultStrBuilder.toString() + "\n");
-                //易生公钥验证返回签名
-                StringUtils.rsaVerifySign(resultStrBuilder, easypay_pub_key);
-            }
-
-        } catch (Exception e) {
-            if (e != null) {
-                System.out.print(e.getMessage() + "\n");
-            } else {
-                System.out.print("-----其他未知错误--------" + "\n");
+            StringBuilder resultStrBuilder = new StringBuilder();
+            int ret = HttpConnectUtils.sendRequest(url, KeyUtils.TEST_DEFAULT_CHARSET, reqMap, 30000, 60000, "POST", resultStrBuilder, null);
+            System.out.print(" \n请求地址为：" + url +
+                    "\n 请求结果为：" + ret +
+                    "\n 请求参数为：" + reqMap.toString() +
+                    "\n 返回内容为：" + resultStrBuilder.toString() + "\n");
+        }catch (Exception e){
+            if(e != null){
+                System.out.print(e.getMessage()+ "\n");
+            }else {
+                System.out.print("-----其他未知错误--------"+ "\n");
             }
         }
     }
-
-
 }
