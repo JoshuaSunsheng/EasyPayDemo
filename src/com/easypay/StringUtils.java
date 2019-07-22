3a3116071bc71bbd028e0b76e72ec8ce67395288
su371128017 <su371128017@163.com>
2019-07-22 14:31:21 +0800

    回调通知报文，验证签名

--- src/com/easypay/StringUtils.java
+++ src/com/easypay/StringUtils.java
@@ -268,4 +268,15 @@ public class StringUtils {
         boolean isTrue=AlipaySignature.rsaCheckContent(returnString, returnSign, easypay_pub_key, "UTF-8");
         System.out.println("验证返回签名是否正确：" + isTrue);
     }
+
+
+    //回调通知报文，验证签名
+    public static void main(String[] args) throws Exception {
+        String msg = "charset=utf-8&biz_content={\"ref_no\":\"11620190722110938208789\",\"notify_type\":\"WAIT_TRIGGER\",\"notify_id\":\"2019072205448161\",\"notify_time\":\"2019-07-22 12:37:24\",\"trade_no\":\"2019072205448161\",\"pay_no\":\"2019072205448161\",\"out_trade_no\":\"2019072200057\",\"payment_type\":\"1\",\"subject\":\"测试商品AAAAA\",\"body\":null,\"total_fee\":\"0.01\",\"amount\":\"0.01\",\"trade_status\":\"TRADE_FINISHED\",\"seller_email\":null,\"seller_id\":\"100000000081484\",\"buyer_id\":\"o8QAHwXd0BLZdYZzDQY0lDoHKnNQ\",\"buyer_email\":\"\",\"gmt_create\":\"2019-07-22 11:09:37\",\"gmt_payment\":\"2019-07-22 11:09:48\",\"is_success\":\"T\",\"is_total_fee_adjust\":\"0\",\"discount\":\"0\",\"gmt_logistics_modify\":\"2019-07-22 11:09:48\",\"price\":\"0.01\",\"quantity\":\"1\",\"seller_actions\":\"SEND_GOODS\"}&partner=100000000081484&sign=ZYgyNrwhKCEbHk9nJ0+XyswMjyBrDjrd9O2/8rV3/w1NTyoxua7UbYSapPB+gGtsK1nxzbBpU2yrKzRaEfPVsgMTP9vDs5rSgb3wALtrPJBqwSsobhQ/FidRFzc5WFt7vJBfdXaAAjuyly22z7QaQ/LEoEYHFa+sh5364/be+tA=&sign_type=RSA";
+
+        String returnString = org.apache.commons.lang.StringUtils.substringBetween(msg,"biz_content=", "&partner=");
+        String returnSign = org.apache.commons.lang.StringUtils.substringBetween(msg,"sign=","&sign_type");
+        boolean isTrue=AlipaySignature.rsaCheckContent(returnString, returnSign, KeyUtils.TEST_EASYPAY_PUBLIC_KEY, "UTF-8");
+        System.out.println("验证返回签名是否正确：" + isTrue);
+    }
 }
