9ee0dd40f9faa75c259c16b7c1906296bac6c60a
su371128017 <su371128017@163.com>
2020-01-20 14:30:09 +0800

    test

--- src/com/easypay/WalletMain.java
+++ src/com/easypay/WalletMain.java
@@ -1,7 +1,191 @@
 package com.easypay;
 
+import net.sf.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by sss on 2019/12/31.
  */
 public class WalletMain {
+    //标记生产还是测试环境
+    public static boolean isTest = true;
+
+    //根据接口文档生成对应的json请求字符串
+    private static String biz_content = "";
+
+    //接口文档中的方法名
+    private static String service = "trade.auth.preauth";
+
+    //商户号
+    private static String merchant_id = KeyUtils.TEST_DEFAULT_MERCHANT_ID;
+
+    //接入机构号
+    private static String partner = KeyUtils.TEST_DEFAULT_PARTNER;
+
+    //请求地址
+    private static String url = KeyUtils.DEFAULT_URL;
+
+    //商户私钥
+    private static String key = KeyUtils.TEST_MERCHANT_PRIVATE_KEY;
+
+    //易生公钥
+    private static String easypay_pub_key = KeyUtils.TEST_EASYPAY_PUBLIC_KEY;
+
+    //加密密钥
+    private static String DES_ENCODE_KEY = KeyUtils.TEST_DES_ENCODE_KEY;
+
+    //查询账户信息
+    public static void getWalletInfo(long userId){
+        JSONObject sParaTemp = new JSONObject();
+        sParaTemp.put("merchant_id", merchant_id);
+        sParaTemp.put("user_id", userId);
+
+        biz_content = sParaTemp.toString();
+        service  = "easypay.wallet.info";
+    }
+    //查询账户交易明细
+    public static void getWalletTradeList(long userId){
+        JSONObject sParaTemp = new JSONObject();
+        sParaTemp.put("merchant_id", merchant_id);
+        sParaTemp.put("user_id", userId);
+        sParaTemp.put("start_date", "2020-01-17");
+        sParaTemp.put("end_date", "2020-01-17");
+        sParaTemp.put("start", 0);
+        sParaTemp.put("page_size", 10);
+        sParaTemp.put("trade_type", 0);
+
+        biz_content = sParaTemp.toString();
+        service  = "easypay.wallet.tradeList";
+    }
+
+    //新无卡-协议支付-支付
+    public static void consume(long userId, String card){
+        JSONObject sParaTemp = new JSONObject();
+        sParaTemp.put("merchant_id", merchant_id);
+        sParaTemp.put("user_id", userId);
+        sParaTemp.put("amount", "1");
+        Date d = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss ");
+        sParaTemp.put("business_time", sdf.format(d) ); //"2019-12-07 15:35:00"
+        sParaTemp.put("notify_url", "https://www.baidu.com");
+        sParaTemp.put("order_desc", "Echannell");
+        sParaTemp.put("out_trade_no", KeyUtils.getOutTradeNo());
+        sParaTemp.put("subject", "subject");
+        sParaTemp.put("password", "11111166");
+        sParaTemp.put("pay_type", 1);//1 钱包 2 银行卡
+        sParaTemp.put("card_no", 1);//1 钱包 2 银行卡
+
+        biz_content = sParaTemp.toString();
+        service  = "easypay.wallet.consume";
+    }
+    //订单查询
+    public static void orderQuery(long userId){
+        JSONObject sParaTemp = new JSONObject();
+        sParaTemp.put("merchant_id", merchant_id);
+        sParaTemp.put("user_id", userId);
+        sParaTemp.put("out_trade_no", "114591_1573558276000");
+        sParaTemp.put("trade_type", 504);
+
+        biz_content = sParaTemp.toString();
+        service  = "easypay.wallet.order.query";
+    }
+
+    //新无卡-协议支付-账户认证
+    public static void validateAccount(long userId){
+        JSONObject sParaTemp = new JSONObject();
+        sParaTemp.put("merchant_id", merchant_id);
+        sParaTemp.put("user_id", userId);
+        sParaTemp.put("name", getEncode("全渠道"));    //账户姓名
+        sParaTemp.put("id_no", getEncode("100030033330030044")); //身份证号
+        sParaTemp.put("card_no", getEncode("6226188887788788"));   //银行卡号
+        sParaTemp.put("mobile", getEncode("137666666666")); //手机号
+        sParaTemp.put("out_trade_no", KeyUtils.getOutTradeNo());
+        biz_content = sParaTemp.toString();
+
+        service  = "easypay.pay.agreement.validate";
+    }
+
+
+    private static String getEncode(String data){
+        return StringUtils.bytesToHexStr(DesUtil.desEncode(data, DES_ENCODE_KEY));
+    }
+
+    //新无卡-协议支付-账户签约
+    public static void agreementPayBind(String orig_out_trade_no, String sms_code){
+        JSONObject sParaTemp = new JSONObject();
+        sParaTemp.put("merchant_id", merchant_id);
+        sParaTemp.put("orig_out_trade_no", orig_out_trade_no);
+        sParaTemp.put("sms_code", sms_code);
+
+        biz_content = sParaTemp.toString();
+        service  = "easypay.pay.agreement.bind";
+    }
+
+    public static void main(String[] args) {
+        //易生请求示例子
+        try {
+
+            //系统入件之后生成的合作伙伴ID（一般会通过邮件发送）
+            if (!isTest) {
+                //商户号
+                merchant_id = KeyUtils.SC_DEFAULT_MERCHANT_ID;
+                //接入机构号
+                partner = KeyUtils.SC_DEFAULT_PARTNER;
+                //请求地址
+                url = KeyUtils.SC_URL;
+                //商户私钥
+                key = KeyUtils.SC_MERCHANT_PRIVATE_KEY;
+                //易生公钥
+                easypay_pub_key = KeyUtils.SC_EASYPAY_PUBLIC_KEY;
+                //加密密钥
+                DES_ENCODE_KEY = KeyUtils.SC_DES_ENCODE_KEY;
+            }
+
+            //账户充值
+//            WalletMain.consume("ES2019041800105873", 8);
+            //账户查询
+            WalletMain.getWalletInfo(116194);
+            //账户明细查询
+//            WalletMain.getWalletTradeList(100701);
+//            WalletMain.orderQuery(100701);
+//            WalletMain.consume(100701);
+//            新无卡-直接支付
+//            NewCardMain.directPay("201906251561446701747", "683222");
+            //加密类型，默认RSA
+            String sign_type = KeyUtils.TEST_DEFAULT_ENCODE_TYPE;
+            //编码类型
+            String charset = KeyUtils.TEST_DEFAULT_CHARSET;
+
+            //根据请求参数生成的机密串
+            String sign = KeyUtils.getSign(key, charset, biz_content);
+            System.out.print("计算签名数据为：" + sign + "\n");
+            Map<String, String> reqMap = new HashMap<String, String>(6);
+            reqMap.put("biz_content", biz_content);
+            reqMap.put("service", service);
+            reqMap.put("partner", partner);
+            reqMap.put("sign_type", sign_type);
+            reqMap.put("charset", charset);
+            reqMap.put("sign", sign);
+
+            StringBuilder resultStrBuilder = new StringBuilder();
+            int ret = HttpConnectUtils.sendRequest(url, KeyUtils.TEST_DEFAULT_CHARSET, reqMap, 30000, 60000, "POST", resultStrBuilder, null);
+            System.out.print(" \n请求地址为：" + url +
+                    "\n 请求结果为：" + ret +
+                    "\n 请求参数为：" + reqMap.toString() +
+                    "\n 返回内容为：" + resultStrBuilder.toString() + "\n");
+            //易生公钥验证返回签名
+            StringUtils.rsaVerifySign(resultStrBuilder, easypay_pub_key);
+        }catch (Exception e){
+            if(e != null){
+                System.out.print(e.getMessage()+ "\n");
+            }else {
+                System.out.print("-----其他未知错误--------"+ "\n");
+            }
+        }
+    }
+
 }
